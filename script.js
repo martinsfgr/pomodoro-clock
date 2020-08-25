1c5678b896158707dafebd846584ec2fa197b68c
Mauricio <mauriciofagner2010@hotmail.com>
2020-08-24 21:44:06 -0300

    Formatar o tempo para aparecer no display

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let clockDisplay = document.querySelector(".clock__display").innerHTML;
+let clockDisplay = document.querySelector(".clock__display");
 let progressBar = document.querySelector(".clock__time-marker");
 let toggleButton = document.querySelector("#start");
 let resetButton = document.querySelector("#reset");
 
 let workSessionDuration = 25 * 60;
-let currentTimeLeftInSession = 25 * 60;
+let currentTimeLeftInSession = workSessionDuration;
 let isClockRunning = false;
 
 toggleButton.addEventListener("click", event => {
@@ -31,6 +31,7 @@ const decreaseTime = () => {
   currentTimeLeftInSession--;
   checkEndOfSession();
   updateProgressBar();
+  updateClockDisplay(currentTimeLeftInSession);
 }
 
 const checkEndOfSession = () => {
@@ -54,7 +55,20 @@ const getPercentageOfTimeElapsed = () => {
   return percentage.toFixed(2);
 }
 
+const updateClockDisplay = (secs) => {
+  let minutes = Math.floor(secs / 60);
+  let seconds = secs % 60;
+
+  const formattedSchedule = [minutes, seconds]
+    .map(value => value > 10 ? value : "0" + value)
+    .join(":");
+  
+  clockDisplay.innerHTML = formattedSchedule;
+}
+
 const updateProgressBar = () => {
   let percentage = getPercentageOfTimeElapsed();
   progressBar.style.width = `${percentage}%`;
 }
+
+updateClockDisplay(workSessionDuration);
